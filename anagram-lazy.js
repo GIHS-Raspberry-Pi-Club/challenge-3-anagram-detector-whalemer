bf192151275400c1314998020a20ee5cc00be05c
jumbowhales <bi.al.ro.bar@gmail.com>
2017-11-08 21:56:54 +1030

    use a different, slightly cheaper hack for string conversion

--- anagram-lazy.js
+++ anagram-lazy.js
@@ -1,4 +1,4 @@
 
-module.exports = (a, b) => ''.split.call(a,'').sort().join('') === ''.split.call(b, '').sort().join('');
+module.exports = (a, b) => `${a}`.split('').sort().join('') === `${b}`.split('').sort().join('');
 //basically sorts each string, then compares them
 //uses call as a cheap hack to force string conversion
